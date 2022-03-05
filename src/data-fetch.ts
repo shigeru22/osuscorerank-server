a098bdd61cb3eac81549525f2d8a9556d4b873f4
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-05 12:05:17 +0700

    Implement UI to data fetch utility

--- src/data-fetch.ts
+++ src/data-fetch.ts
@@ -1,13 +1,61 @@
+/* eslint-disable operator-linebreak */
+
 import env from "dotenv";
-import _ from "lodash";
+import inquirer from "inquirer";
 import fs from "fs";
+import _ from "lodash";
 import { getAccessToken, getScoreRanking, revokeAccessToken } from "./utils/osu-api/osu";
 import { LogLevel, log } from "./utils/log";
 import { checkNumber } from "./utils/common";
 
 env.config();
 
+function importData() {
+	console.log("Import data");
+}
+
 async function fetchData() {
+	/* linebreak rule disabled for code readibility purpose */
+
+	console.log(
+		"Please note: osu! API rate is limited. Although the rate is (nicely) high (about\n" +
+		"1200 requests per minute), it's strongly advised to keep it slow to prevent\n" +
+		"overusages. This program will fetch a single request every second.\n" +
+		"Rankings from the API are limited up to 200 pages, with total of 10000 users\n" +
+		"that would would be fetched.\n" +
+		"Also note that the API has no total count in its Cursor object, no progress will\n" +
+		"be shown other than how many data has been processed, so be patient."
+	);
+
+	const ansContinue = await inquirer.prompt({
+		type: "confirm",
+		name: "continue",
+		message: "Continue?",
+		default: false
+	});
+
+	if(!ansContinue.continue) {
+		return;
+	}
+
+	const ansCountry = await inquirer.prompt({
+		type: "input",
+		name: "countryCode",
+		message: "Enter country code: ",
+		validate: (inp) => {
+			const temp = inp as string;
+			if(temp.length !== 2) {
+				console.log(
+					"\nCountry code must be in 2-letter code ISO 3166-1 Alpha-2 format.\n" +
+					"Listing could be found at:\n" +
+					"https://en.wikipedia.org/w/index.php?title=List_of_ISO_3166_country_codes#Current_ISO_3166_country_codes"
+				);
+				return false;
+			}
+			return true;
+		}
+	});
+
 	log("Checking for environment variables...");
 	if(_.isUndefined(process.env.OSU_CLIENT_ID) || _.isUndefined(process.env.OSU_CLIENT_SECRET)) {
 		log("OSU_CLIENT_ID or OSU_CLIENT_SECRET must not be empty. Exiting.", LogLevel.ERROR);
@@ -25,6 +73,9 @@ async function fetchData() {
 
 	log(`Access token: ${ token }`, LogLevel.DEBUG);
 
+	const startTime = new Date();
+
+	log("Retrieving rankings...");
 	const rankings = await getScoreRanking(token);
 
 	if(_.isNull(rankings)) {
@@ -36,6 +87,8 @@ async function fetchData() {
 				fs.mkdirSync("./dist");
 			}
 			fs.writeFileSync("./dist/rankings.json", JSON.stringify(rankings, null, 2));
+
+			log("Rankings saved to /dist/rankings.json.");
 		}
 		catch (e) {
 			if(_.isError(e)) {
@@ -54,6 +107,51 @@ async function fetchData() {
 	else {
 		log("Unable to revoke access token.", LogLevel.WARN);
 	}
+
+	const endTime = new Date();
+
+	log(`Country rankings fetched in ${ ((endTime.getTime() / 1000) - (startTime.getTime() / 1000)).toFixed(3) } seconds.`);
 }
 
-fetchData();
+async function main() {
+	const menus = [
+		"Fetch data from osu! API",
+		"Import dist/rankings.json to database",
+		"Exit"
+	];
+
+	try {
+		console.clear();
+
+		const answer = await inquirer.prompt([
+			{
+				type: "list",
+				name: "selection",
+				message: "Select an option:",
+				choices: menus
+			}
+		]);
+
+		const inp = _.findIndex(menus, item => _.isEqual(item, answer.selection));
+
+		switch(inp) {
+			case 0: await fetchData(); break;
+			case 1: await importData(); break;
+			case 2: break;
+		}
+
+		process.exit(0);
+	}
+	catch (e) {
+		if(_.isError(e)) {
+			log(`Error occurred.\n${ e.name }: ${ e.message }`, LogLevel.ERROR);
+		}
+		else {
+			log("Unknown error occurred. Exiting...", LogLevel.ERROR);
+		}
+
+		process.exit(1);
+	}
+}
+
+main();
