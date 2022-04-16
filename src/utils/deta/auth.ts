ac4ef0735e1bfcafd031e24a4176427ef3aab8aa
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-16 14:00:54 +0700

    Implement insert client function

--- src/utils/deta/auth.ts
+++ src/utils/deta/auth.ts
@@ -1,7 +1,8 @@
 import _ from "lodash";
 import Deta from "deta/dist/types/deta";
-import { IClientDetailData } from "../../types/auth";
+import { IClientData, IClientDetailData } from "../../types/auth";
 import { LogLevel, log } from "../log";
+import { randomBytes } from "crypto";
 
 const DB_NAME = "client-auth";
 
@@ -22,12 +23,54 @@ export async function getClientById(deta: Deta, id: string) {
 	}
 	catch (e) {
 		if(_.isError(e)) {
-			log(`getScores :: ${ e.name }: ${ e.message }\n${ e.stack }`, LogLevel.ERROR);
+			log(`getClientById :: ${ e.name }: ${ e.message }\n${ e.stack }`, LogLevel.ERROR);
 		}
 		else {
-			log("getScores :: Unknown error occurred.", LogLevel.ERROR);
+			log("getClientById :: Unknown error occurred.", LogLevel.ERROR);
 		}
 
 		return null;
+	}
+}
+
+export async function insertClient(deta: Deta, id: string, name: string) {
+	const db = deta.Base(DB_NAME);
+
+	try {
+		let currentLastId = 0;
+		{
+			const rows = (await db.fetch()).items as unknown as IClientDetailData[];
+			if(rows.length > 0) {
+				currentLastId = _.parseInt(rows[rows.length - 1].key, 10);
+			}
+		}
+
+		const data: IClientData = {
+			clientId: id,
+			clientName: name,
+			clientKey: randomBytes(64).toString("hex")
+		};
+
+		const date = new Date();
+
+		await db.put({
+			clientId: data.clientId,
+			clientName: data.clientName,
+			clientKey: data.clientKey,
+			dateAdded: date.toISOString()
+		}, (currentLastId + 1).toString());
+
+		log(`addClient :: ${ DB_NAME }: inserted 1 row.`, LogLevel.LOG);
+		return true;
+	}
+	catch (e) {
+		if(_.isError(e)) {
+			log(`addClient :: ${ e.name }: ${ e.message }\n${ e.stack }`, LogLevel.ERROR);
+		}
+		else {
+			log("addClient :: Unknown error occurred.", LogLevel.ERROR);
+		}
+
+		return false;
 	}
 }
