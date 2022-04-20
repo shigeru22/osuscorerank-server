449103991919eb3b0cd0534da85ec84e5aa727d3
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-20 17:26:48 +0700

    Ignore dist directory from linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended"
 	],
+	"ignorePatterns": [ "dist/**/*.js" ],
 	"parser": "@typescript-eslint/parser",
 	"parserOptions": {
 		"ecmaFeatures": {
