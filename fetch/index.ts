bbc1ff5c0714dc3a8557a337b1201f3dbb1beaf3
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-23 23:49:57 +0700

    Implement main menu UI

--- fetch/index.ts
+++ fetch/index.ts
@@ -1,5 +1,17 @@
-function main() {
-	console.log("Hello, world!");
+import { inputNumber, inputText } from "./input";
+
+async function main() {
+	console.clear();
+
+	console.log("osu! Score Rank API Data Fetcher\n");
+	console.log("  1. Fetch data from osu! API");
+	console.log("  2. Import data from temp/rankings.json");
+	console.log("  0. Exit\n");
+
+	const input = await inputNumber("Input:", id => id >= 0 && id <= 2);
+	switch(input) {
+		case 0: process.exit(0);
+	}
 }
 
 main();
