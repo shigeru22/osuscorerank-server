74f90a7d84d916dd6453dd8b4a7f81833f759aae
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-23 23:30:23 +0700

    Implement input module

---
+++ fetch/input.ts
@@ -0,0 +1,68 @@
+/* eslint-disable consistent-return */
+
+import inquirer from "inquirer";
+import _ from "lodash";
+
+export async function inputNumber(promptText: string, validCallback: (ret: number) => boolean): Promise<number> {
+	try {
+		let condition = false;
+		let ret = 0;
+
+		while(!condition) {
+			// eslint-disable-next-line no-await-in-loop
+			const input = await inquirer.prompt([
+				{
+					name: "data",
+					message: promptText
+				}
+			]);
+
+			const tempNumber = _.parseInt(input.data, 10);
+			if(_.isNaN(tempNumber)) {
+				console.log("Input is not number.");
+			}
+			else if(!validCallback(tempNumber)) {
+				console.log("Input is not valid.");
+			}
+			else {
+				ret = tempNumber;
+				condition = true;
+			}
+		}
+
+		return ret;
+	}
+	catch (e) {
+		if(_.isError(e)) {
+			console.log(`${ e.name }: ${ e.message }`);
+		}
+		else {
+			console.log("Unknown error occurred.");
+		}
+
+		process.exit(1);
+	}
+}
+
+export async function inputText(promptText: string): Promise<string> {
+	try {
+		const input = await inquirer.prompt([
+			{
+				name: "data",
+				message: promptText
+			}
+		]);
+
+		return input.data;
+	}
+	catch (e) {
+		if(_.isError(e)) {
+			console.log(`${ e.name }: ${ e.message }`);
+		}
+		else {
+			console.log("Unknown error occurred.");
+		}
+
+		process.exit(1);
+	}
+}
